100768a8b71a336530c300502cd387eaf316cee2
Jijumon <95122104+jijuGM@users.noreply.github.com>
2025-07-10 00:01:01 +0530

    Delete train_model.py

--- train_model.py
+++
@@ -1,9 +0,0 @@
-from sklearn.datasets import load_iris
-from sklearn.linear_model import LogisticRegression
-import joblib
-
-X, y = load_iris(return_X_y=True)
-model = LogisticRegression()
-model.fit(X, y)
-
-joblib.dump(model, "model.pkl")
